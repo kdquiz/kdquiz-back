93a8d662e4f9918a7f11dee7c6aaae4b7f310703
fallkim <fs728kimgc@naver.com>
2024-03-25 20:11:20 +0900

    초기세팅

---
+++ src/main/java/kdquiz/KdquizApplication.java
@@ -0,0 +1,13 @@
+package kdquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdquizApplication.class, args);
+	}
+
+}
